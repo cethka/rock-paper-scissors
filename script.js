e5b83515289438aaec97326ed3e2c28054b2a2f3
cethka <ceyhuntaha@protonmail.com>
2025-05-18 17:02:35 +0300

    Make the computer choose its move

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+// Make the computer choose its move
+
+function getComputerChoice() {
+  const choiceNum = Math.floor(Math.random() * 3);
+  switch(choiceNum) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+  // No need for break statements since we're returning a value after each case statement anyway
+}
+
+// console.log(getComputerChoice());
+
+// Let the user choose their move
+// Compare the results
+// Announce the winner
